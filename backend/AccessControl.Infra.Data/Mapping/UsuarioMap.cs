70be9d68b3ca6c389d4cd1d36fbb698504af8d9d
Claudio Santana <claudio.a.santana@outlook.com>
2025-01-15 11:33:15 -0300

    Deploy ajustado com sucesso.

---
+++ backend/AccessControl.Infra.Data/Mapping/UsuarioMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessControl.Infra.Data.Mapping
+{
+    public class UsuarioMap
+    {
+    }
+}
