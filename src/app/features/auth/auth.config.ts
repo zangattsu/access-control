4e6bc84a0eee78df49697534b5da604d3c045d7e
Claudio Santana <claudio_desenv@hotmail.com>
2025-04-29 01:49:33 -0300

    Correção do redirectUri.

--- src/app/features/auth/auth.config.ts
+++ src/app/features/auth/auth.config.ts
@@ -1,12 +1,11 @@
 import { OktaAuthOptions } from '@okta/okta-auth-js';
-import { WindowDefinition } from '../../shared/window.definition';
 
 export const oktaAuthConfig: OktaAuthOptions = {
   // Configurações do Okta Auth
   // url: 'https://dev-67094057.okta.com/oauth2/default', // URL do servidor de autorização Okta  
   issuer: 'https://dev-67094057.okta.com/oauth2/default',
   clientId: '0oao9ep7pfvFyPrzU5d7',
-  redirectUri: 'http/localhost:4200/login/callback',
+  redirectUri: 'http://localhost:4200/login/callback',
   scopes: ['openid', 'profile', 'email'],
   pkce: true, // Proof Key for Code Exchange (PKCE) - obrigatório para Authorization Code Flow
   responseMode: 'query',
