1f88fccacf60aaa25cda9afd3f8fdf71e31f5289
Claudio Santana <claudio_desenv@hotmail.com>
2025-04-30 00:46:24 -0300

    Ajusta nas configurações Okta: policy in application

--- src/app/features/auth/auth.config.ts
+++ src/app/features/auth/auth.config.ts
@@ -3,11 +3,8 @@ import { OktaAuthOptions } from '@okta/okta-auth-js';
 export const oktaAuthConfig: OktaAuthOptions = {
   // Configurações do Okta Auth
   // url: 'https://dev-67094057.okta.com/oauth2/default', // URL do servidor de autorização Okta  
-  issuer: 'https://dev-67094057.okta.com/oauth2/default',
-  clientId: '0oao9ep7pfvFyPrzU5d7',
+  issuer: 'https://dev-21512408.okta.com/oauth2/default',
+  clientId: '0oaoijpj1t9qFm7HC5d7',
   redirectUri: 'http://localhost:4200/login/callback',
-  scopes: ['openid', 'profile', 'email'],
-  pkce: true, // Proof Key for Code Exchange (PKCE) - obrigatório para Authorization Code Flow
-  responseMode: 'query',
-  responseType: 'code'
+  scopes: ['openid', 'profile', 'email']
 };
